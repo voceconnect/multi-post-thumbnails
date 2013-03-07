dcc0ed185a15673e894a12651bdd6c0a51f03ca7
Chris Scott <chris@iamzed.com>
2013-03-07 16:57:26 -0500

    Fix il8n to use correct directory

--- multi-post-thumbnails.php
+++ multi-post-thumbnails.php
@@ -416,5 +416,5 @@ if (!class_exists('MultiPostThumbnails')) {
 	}
 
 	if ( is_admin() )
-		load_plugin_textdomain( 'multiple-post-thumbnails', FALSE, 'multi-post-thumbnails/languages/' );
+		load_plugin_textdomain( 'multiple-post-thumbnails', FALSE, 'multiple-post-thumbnails/languages/' );
 }
