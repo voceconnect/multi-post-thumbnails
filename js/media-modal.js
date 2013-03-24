2b520a7cf951f2f0e808d5c214c213d8d5138cec
Chris Scott <cscott@voceconnect.com>
2013-03-24 17:11:35 -0400

    don't reuse frames

--- js/media-modal.js
+++ js/media-modal.js
@@ -14,37 +14,35 @@ var MediaModal = function (options) {
 
     this.openFrame = function openFrame(e) {
 		e.preventDefault();
+
+		// Create the media frame.
+		frame = wp.media.frames.file_frame = wp.media({
+			title: jQuery(this).data('uploader_title'),
+			button: {
+				text: jQuery(this).data('uploader_button_text')
+			},
+			multiple: false
+		});
+
+		// When an image is selected, run a callback.
+		frame.on('select', function () {
+			// We set multiple to false so only get one image from the uploader
+			var attachment = frame.state().get('selection').first().toJSON();
+			that.settings.cb(attachment);
+		});
+
+		frame.on('open activate', function() {
+			// Get the link/button/etc that called us
+			var $caller = jQuery(that.settings.calling_selector);
+
+			// Select the thumbnail if we have one
+			if ($caller.data('thumbnail_id')) {
+				var Attachment = wp.media.model.Attachment;
+				var selection = frame.state().get('selection');
+				selection.add(Attachment.get($caller.data('thumbnail_id')));
+			}
+		});
         
-        if (!frame) {
-			// Create the media frame.
-			frame = wp.media.frames.file_frame = wp.media({
-				title: jQuery(this).data('uploader_title'),
-				button: {
-					text: jQuery(this).data('uploader_button_text')
-				},
-				multiple: false
-			});
-			
-			// When an image is selected, run a callback.
-			frame.on('select', function () {
-				// We set multiple to false so only get one image from the uploader
-				var attachment = frame.state().get('selection').first().toJSON();
-				that.settings.cb(attachment);
-			});
-			
-			frame.on('open activate', function() {
-				// Get the link/button/etc that called us
-				var $caller = jQuery(that.settings.calling_selector);
-				
-				// Select the thumbnail if we have one
-				if ($caller.data('thumbnail_id')) {
-					var Attachment = wp.media.model.Attachment;
-					var selection = frame.state().get('selection');
-					selection.add(Attachment.get($caller.data('thumbnail_id')));
-				}
-			});
-        }
-		
         frame.open();
     };
 
