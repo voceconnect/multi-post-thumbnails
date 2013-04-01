55f1e401758f8f17089599cc272ff52327bdebba
Chris Scott <cscott@voceconnect.com>
2013-04-01 11:04:02 -0400

    enqueue media scripts to handle post types that don't have 'thumbnail' support

--- multi-post-thumbnails.php
+++ multi-post-thumbnails.php
@@ -180,7 +180,8 @@ if (!class_exists('MultiPostThumbnails')) {
 				add_thickbox();
 				wp_enqueue_script( "mpt-featured-image", $this->plugins_url( 'js/multi-post-thumbnails-admin.js', __FILE__ ), array( 'jquery', 'media-upload' ) );
 			} else { // 3.5+ media modal
-				wp_enqueue_script( "mpt-featured-image", $this->plugins_url( 'js/multi-post-thumbnails-admin.js', __FILE__ ), array( 'jquery', 'media-upload', 'set-post-thumbnail' ) );
+				wp_enqueue_media();
+				wp_enqueue_script( "mpt-featured-image", $this->plugins_url( 'js/multi-post-thumbnails-admin.js', __FILE__ ), array( 'jquery', 'set-post-thumbnail' ) );
 				wp_enqueue_script( "mpt-featured-image-modal", $this->plugins_url( 'js/media-modal.js', __FILE__ ), array( 'jquery', 'media-models' ) );				
 			}
 		}
