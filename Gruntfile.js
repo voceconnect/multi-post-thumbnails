bc6997a69ca7240388f645b144648ff6732a857e
Chris Scott <cscott@voceconnect.com>
2014-06-27 15:44:27 -0400

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+/*
+ * multi-post-thumbnails 
+ * https://github.com/voceconnect/multi-post-thumbnails
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    "build": {
+      "production": [],
+    }
+  });
+
+  //load the tasks
+  grunt.loadNpmTasks('grunt-voce-plugins');
+
+};
