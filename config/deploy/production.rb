9851f080238a7d56c0ca54e0901daaa83cba0560
Chris Scott <cscott@voceconnect.com>
2014-06-27 15:27:02 -0400

    add Capistrano files

---
+++ config/deploy/production.rb
@@ -0,0 +1,14 @@
+set :application, 'voce-seo'
+set :repo_url, "git@github.com:voceconnect/#{fetch(:application)}.git"
+
+set :scm, 'git-to-svn'
+set :type, 'plugin'
+
+set :svn_repository, "http://plugins.svn.wordpress.org/multiple-post-thumbnails/"
+set :svn_deploy_to, "trunk"
+
+set :build_folders, (
+  fetch(:build_folders) << %w{
+    *config
+  }
+).flatten
