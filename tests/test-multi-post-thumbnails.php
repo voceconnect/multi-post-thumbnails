3935212afd8973a5a0e456210e672912cc6c28b7
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-07-30 20:07:54 -0400

    Add initial unit testing framework.

---
+++ tests/test-multi-post-thumbnails.php
@@ -0,0 +1,11 @@
+<?php
+
+class MultiPostThumbnails_TestCase extends WP_UnitTestCase {
+
+	function testSample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+
+}
+
