e30697b4e8315697dfa7de341bd8761367d9c2aa
Chris Scott <chris@iamzed.com>
2015-11-06 17:49:45 -0500

    Load text domain for translations directly to allow for plugin to be outside WP_PLUGIN_DIR or WP_LANG_DIR.

--- multi-post-thumbnails.php
+++ multi-post-thumbnails.php
@@ -464,6 +464,11 @@ if (!class_exists('MultiPostThumbnails')) {
 
 	}
 
-	if ( is_admin() )
-		load_plugin_textdomain( 'multiple-post-thumbnails', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+	if ( is_admin() ) {
+		$domain = 'multiple-post-thumbnails';
+		$locale = apply_filters('plugin_locale', get_locale(), $domain);
+		$mofile = $domain . '-' . $locale . '.mo';
+
+		load_textdomain( $domain, dirname( __FILE__ ) . '/languages/' . $mofile );
+	}
 }
