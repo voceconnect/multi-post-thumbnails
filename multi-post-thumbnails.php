8bad6faca5be3da4b900e4afce20d2a3936145e8
dumitrub <brinzan.dumitru@gmail.com>
2017-01-03 08:24:50 +0100

    Proposed fix to issue #113.
    Proposed fix to issue #113, specifically a fix to code not working on WP installs with multiple post types.
    https://github.com/voceconnect/multi-post-thumbnails/issues/113

--- multi-post-thumbnails.php
+++ multi-post-thumbnails.php
@@ -3,9 +3,10 @@
 Plugin Name: Multiple Post Thumbnails
 Plugin URI: http://wordpress.org/extend/plugins/multiple-post-thumbnails/
 Description: Adds the ability to add multiple post thumbnails to a post type.
-Version: 1.6.6
+Version: 1.7.0
 Author: Chris Scott
 Author URI: http://voceplatforms.com/
+Version 1.7.0 (WordPress 4.5+ bugfix) by Dumitru Brinzan (http://www.brinzan.com/)
 */
 
 /*  Copyright 2010 Chris Scott (cscott@voceconnect.com)
@@ -28,14 +29,6 @@ Author URI: http://voceplatforms.com/
 if (!class_exists('MultiPostThumbnails')) {
 
 	class MultiPostThumbnails {
-
-		/**
-		 * Records if the scripts and styles have been enqueued so that we only
-		 * do so once.
-		 *
-		 * @var boolean
-		 */
-		protected static $statics_enqueued = false;
 
 		public function __construct($args = array()) {
 			$this->register($args);
@@ -93,13 +86,16 @@ if (!class_exists('MultiPostThumbnails')) {
 			}
 
 			add_action('add_meta_boxes', array($this, 'add_metabox'));
-			if (version_compare($wp_version, '3.5', '<')) {				
-				add_filter('attachment_fields_to_edit', array($this, 'add_attachment_field'), 20, 2);
-			}
 			add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
 			add_action('admin_print_scripts-post.php', array($this, 'admin_header_scripts'));
 			add_action('admin_print_scripts-post-new.php', array($this, 'admin_header_scripts'));
-			add_action("wp_ajax_set-{$this->post_type}-{$this->id}-thumbnail", array($this, 'set_thumbnail'));
+			if (is_array($this->post_type)) {
+				foreach ($this->post_type as $post_type) {
+					add_action("wp_ajax_set-{$post_type}-{$this->id}-thumbnail", array($this, 'set_thumbnail'));
+				}
+			} else {
+				add_action("wp_ajax_set-{$this->post_type}-{$this->id}-thumbnail", array($this, 'set_thumbnail'));
+			}
 			add_action('delete_attachment', array($this, 'action_delete_attachment'));
 			add_filter('is_protected_meta', array($this, 'filter_is_protected_meta'), 20, 2);
 		}
@@ -110,7 +106,9 @@ if (!class_exists('MultiPostThumbnails')) {
 		 * @return string 
 		 */
 		public function get_meta_key() {
-			return "{$this->post_type}_{$this->id}_thumbnail_id";
+			global $post_ID;
+			$post_type = get_post_type($post_ID);
+			return "{$post_type}_{$this->id}_thumbnail_id";
 		}
 
 		/**
@@ -119,7 +117,9 @@ if (!class_exists('MultiPostThumbnails')) {
 		 * @return void
 		 */
 		public function add_metabox() {
-			add_meta_box("{$this->post_type}-{$this->id}", __($this->label, 'multiple-post-thumbnails'), array($this, 'thumbnail_meta_box'), $this->post_type, $this->context, $this->priority);
+			global $post_ID;
+			$post_type = get_post_type($post_ID);
+			add_meta_box("{$post_type}-{$this->id}", __($this->label, 'multiple-post-thumbnails'), array($this, 'thumbnail_meta_box'), $post_type, $this->context, $this->priority);
 		}
 
 		/**
@@ -135,53 +135,11 @@ if (!class_exists('MultiPostThumbnails')) {
 		}
 
 		/**
-		 * Throw this in the media attachment fields
-		 *
-		 * @param string $form_fields
-		 * @param string $post
-		 * @return void
-		 */
-		public function add_attachment_field($form_fields, $post) {
-			$calling_post_id = 0;
-			if (isset($_GET['post_id']))
-				$calling_post_id = absint($_GET['post_id']);
-			elseif (isset($_POST) && count($_POST)) // Like for async-upload where $_GET['post_id'] isn't set
-				$calling_post_id = $post->post_parent;
-
-			if (!$calling_post_id)
-				return $form_fields;
-
-			// check the post type to see if link needs to be added
-			$calling_post = get_post($calling_post_id);
-			if (is_null($calling_post) || $calling_post->post_type != $this->post_type) {
-				return $form_fields;
-			}
-
-			$referer = wp_get_referer();
-			$query_vars = wp_parse_args(parse_url($referer, PHP_URL_QUERY));
-			
-			if( (isset($_REQUEST['context']) && $_REQUEST['context'] != $this->id) || (isset($query_vars['context']) && $query_vars['context'] != $this->id) )
-				return $form_fields;
-
-			$ajax_nonce = wp_create_nonce("set_post_thumbnail-{$this->post_type}-{$this->id}-{$calling_post_id}");
-			$link = sprintf('<a id="%4$s-%1$s-thumbnail-%2$s" class="%1$s-thumbnail" href="#" onclick="MultiPostThumbnails.setAsThumbnail(\'%2$s\', \'%1$s\', \'%4$s\', \'%5$s\');return false;">' . __( 'Set as %3$s', 'multiple-post-thumbnails' ) . '</a>', $this->id, $post->ID, $this->label, $this->post_type, $ajax_nonce);
-			$form_fields["{$this->post_type}-{$this->id}-thumbnail"] = array(
-				'label' => $this->label,
-				'input' => 'html',
-				'html' => $link);
-			return $form_fields;
-		}
-
-		/**
 		 * Enqueue admin JavaScripts
 		 *
 		 * @return void
 		 */
 		public function enqueue_admin_scripts( $hook ) {
-			if ( self::$statics_enqueued ) {
-				return;
-			}
-
 			global $wp_version, $post_ID;
 			
 			// only load on select pages
@@ -198,8 +156,6 @@ if (!class_exists('MultiPostThumbnails')) {
 			}
 			
 			wp_enqueue_style( "mpt-admin-css", $this->plugins_url( 'css/multi-post-thumbnails-admin.css', __FILE__ ) );
-
-			self::$statics_enqueued = true;
 		}
 		
 		public function admin_header_scripts() {
@@ -247,15 +203,14 @@ if (!class_exists('MultiPostThumbnails')) {
 		 * @return string the URL of the plugin file
 		 */
 		private function plugins_url($relative_path, $plugin_path) {
-			$template_dir = get_stylesheet_directory();
+			$template_dir = get_template_directory();
 
 			foreach ( array('template_dir', 'plugin_path') as $var ) {
 				$$var = str_replace('\\' ,'/', $$var); // sanitize for Win32 installs
 				$$var = preg_replace('|/+|', '/', $$var);
 			}
-
 			if(0 === strpos($plugin_path, $template_dir)) {
-				$url = get_stylesheet_directory_uri();
+				$url = get_template_directory_uri();
 				$folder = str_replace($template_dir, '', dirname($plugin_path));
 				if ( '.' != $folder ) {
 					$url .= '/' . ltrim($folder, '/');
@@ -342,7 +297,8 @@ if (!class_exists('MultiPostThumbnails')) {
 		 * @return int
 		 */
 		public static function get_post_thumbnail_id($post_type, $id, $post_id) {
-			return get_post_meta($post_id, "{$post_type}_{$id}_thumbnail_id", true);
+			$post_type_new = get_post_type($post_id);
+			return get_post_meta($post_id, "{$post_type_new}_{$id}_thumbnail_id", true);
 		}
 
 		/**
@@ -381,9 +337,10 @@ if (!class_exists('MultiPostThumbnails')) {
 		 */
 		private function post_thumbnail_html($thumbnail_id = null) {
 			global $content_width, $_wp_additional_image_sizes, $post_ID, $wp_version;
+			$post_type = get_post_type($post_ID);
 			
 			$url_class = "";
-			$ajax_nonce = wp_create_nonce("set_post_thumbnail-{$this->post_type}-{$this->id}-{$post_ID}");
+			$ajax_nonce = wp_create_nonce("set_post_thumbnail-{$post_type}-{$this->id}-{$post_ID}");
 			
 			if (version_compare($wp_version, '3.5', '<')) {
 				// Use the old thickbox for versions prior to 3.5
@@ -401,11 +358,11 @@ if (!class_exists('MultiPostThumbnails')) {
 							MultiPostThumbnails.setAsThumbnail(attachment.id, "%2$s", "%1$s", "%4$s");
 						}
 					});',
-					$this->post_type, $this->id, md5($this->id), $ajax_nonce
+					$post_type, $this->id, md5($this->id), $ajax_nonce
 				);
 			}
 			$format_string = '<p class="hide-if-no-js"><a title="%1$s" href="%2$s" id="set-%3$s-%4$s-thumbnail" class="%5$s" data-thumbnail_id="%7$s" data-uploader_title="%1$s" data-uploader_button_text="%1$s">%%s</a></p>';
-			$set_thumbnail_link = sprintf( $format_string, sprintf( esc_attr__( "Set %s" , 'multiple-post-thumbnails' ), $this->label ), esc_url($image_library_url), $this->post_type, $this->id, $url_class, $this->label, $thumbnail_id );
+			$set_thumbnail_link = sprintf( $format_string, sprintf( esc_attr__( "Set %s" , 'multiple-post-thumbnails' ), $this->label ), esc_url($image_library_url), $post_type, $this->id, $url_class, $this->label, $thumbnail_id );
 			$content = sprintf( $set_thumbnail_link, sprintf( esc_html__( "Set %s", 'multiple-post-thumbnails' ), $this->label ) );
 
 			if ($thumbnail_id && get_post($thumbnail_id)) {
@@ -413,16 +370,16 @@ if (!class_exists('MultiPostThumbnails')) {
 				$content_width = 266;
 				$attr = array( 'class' => 'mpt-thumbnail' );
 				
-				if ( !isset($_wp_additional_image_sizes["{$this->post_type}-{$this->id}-thumbnail"])) {
+				if ( !isset($_wp_additional_image_sizes["{$post_type}-{$this->id}-thumbnail"])) {
 						$thumbnail_html = wp_get_attachment_image( $thumbnail_id, array($content_width, $content_width), false, $attr );
 				} else {
-						$thumbnail_html = wp_get_attachment_image( $thumbnail_id, "{$this->post_type}-{$this->id}-thumbnail", false, $attr );
+						$thumbnail_html = wp_get_attachment_image( $thumbnail_id, "{$post_type}-{$this->id}-thumbnail", false, $attr );
 				}
 				
 				if (!empty($thumbnail_html)) {
 					$content = sprintf($set_thumbnail_link, $thumbnail_html);
 					$format_string = '<p class="hide-if-no-js"><a href="#" id="remove-%1$s-%2$s-thumbnail" onclick="MultiPostThumbnails.removeThumbnail(\'%2$s\', \'%1$s\', \'%4$s\');return false;">%3$s</a></p>';
-					$content .= sprintf( $format_string, $this->post_type, $this->id, sprintf( esc_html__( "Remove %s", 'multiple-post-thumbnails' ), $this->label ), $ajax_nonce );
+					$content .= sprintf( $format_string, $post_type, $this->id, sprintf( esc_html__( "Remove %s", 'multiple-post-thumbnails' ), $this->label ), $ajax_nonce );
 				}
 				$content_width = $old_content_width;
 			}
@@ -431,7 +388,7 @@ if (!class_exists('MultiPostThumbnails')) {
 				$content .= sprintf('<script>%s</script>', $modal_js);
 			}
 
-			return apply_filters( sprintf( '%s_%s_admin_post_thumbnail_html', $this->post_type, $this->id ), $content, $post_ID, $thumbnail_id );
+			return apply_filters( sprintf( '%s_%s_admin_post_thumbnail_html', $post_type, $this->id ), $content, $post_ID, $thumbnail_id );
 		}
 
 		/**
@@ -445,8 +402,9 @@ if (!class_exists('MultiPostThumbnails')) {
 			if ( !current_user_can('edit_post', $post_ID))
 				die('-1');
 			$thumbnail_id = intval($_POST['thumbnail_id']);
+			$post_type = get_post_type($post_ID);
 
-			check_ajax_referer("set_post_thumbnail-{$this->post_type}-{$this->id}-{$post_ID}");
+			check_ajax_referer("set_post_thumbnail-{$post_type}-{$this->id}-{$post_ID}");
 
 			if ($thumbnail_id == '-1') {
 				delete_post_meta($post_ID, $this->get_meta_key());
@@ -456,7 +414,7 @@ if (!class_exists('MultiPostThumbnails')) {
 			if ($thumbnail_id && get_post($thumbnail_id)) {
 				$thumbnail_html = wp_get_attachment_image($thumbnail_id, 'thumbnail');
 				if (!empty($thumbnail_html)) {
-					$this->set_meta($post_ID, $this->post_type, $this->id, $thumbnail_id);
+					$this->set_meta($post_ID, $post_type, $this->id, $thumbnail_id);
 					die($this->post_thumbnail_html($thumbnail_id));
 				}
 			}
@@ -479,11 +437,6 @@ if (!class_exists('MultiPostThumbnails')) {
 
 	}
 
-	if ( is_admin() ) {
-		$domain = 'multiple-post-thumbnails';
-		$locale = apply_filters('plugin_locale', get_locale(), $domain);
-		$mofile = $domain . '-' . $locale . '.mo';
-
-		load_textdomain( $domain, dirname( __FILE__ ) . '/languages/' . $mofile );
-	}
+	if ( is_admin() )
+		load_plugin_textdomain( 'multiple-post-thumbnails', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
